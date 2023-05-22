f204b57701491af6b6f38ec197ee49116b6e5b45
youghwjdkill1 <youghwjdkill1@gmail.com>
2023-05-22 20:26:46 +0900

    main

---
+++ exRelational1.py
@@ -0,0 +1,42 @@
+"""
+파일명:exRelational1
+프로그램 설명 : 관계 연산자 예제 1
+"""
+
+a, b = 2, 8
+print(a, b)     # 2,8
+print(a == b)   # False
+print(a != b)   # True
+print(a > b)    # False
+print(a < b)    # True
+print(a >= b)   # False
+print(a <= b)   # True
+
+
+
+
+# 3. 관계 연산자 (Relational operators)
+# 두 값을 비교하기 위한 연산자
+# 관계 연산자 or 비교 연산자라고도 말한다.
+
+# 관계 연산자는 좌변과 우변의 값을 비교해서 크기 관계를 평가하며
+# 그 결과를 참(True), 거짓(False)으로 나타낸다.
+# 결과: True, False
+
+# 형식: 
+# a는 변수, 값, 함수(메소드)오고 b 는 변수, 값, 함수(메소드)를 사용한다.
+# a 관계연산자 b  -> 결과는 True/False
+
+# C++, Java : true, false
+# Python : True, False   
+
+
+# a,b = 2,8 이라고 할 때 
+
+# 연산자     사용 예     의미 설명
+# ==        a == b    같다 (동등) (Equal)
+# !=        a != b    같지 않다   (Not equal)
+# >         a > b     크다 (초과) (Greater than)
+# <         a < b     작다 (미만) (Less than)
+# >=        a >= b    크거나 같다 (이상) (Greater than or equal to)
+# <=        a <= b    작거나 같다 (이하) (Less than or equal to)
