7a7fb12adb641d0bf19abc62a789520f92941653
youghwjdkill1 <youghwjdkill1@gmail.com>
2023-05-22 21:25:07 +0900

    main

---
+++ exMembership1.py
@@ -0,0 +1,15 @@
+"""
+파일명: exMembership1.py 
+프로그램 설명:  멤버쉽 연산자 예제 1
+"""
+
+# 리스트 변수 number에 1 ~ 5까지 저장한다
+number = [1, 2, 3, 4, 5]
+
+# 반복이 가능한지 확인한다.
+print(iter([]), iter(number))
+
+# number 변수에 5가 존재합니까 ?
+print(5 in number)   #True
+# number 변수에 13이 존재합니까 ?
+print(13 in number)  #True
