637d2e55515353395a944678573fb9529917511d
youghwjdkill1 <youghwjdkill1@gmail.com>
2023-09-19 18:36:14 +0900

    20230919

---
+++ 독학/이거저것하는거/20230919 복습.py
@@ -0,0 +1,16 @@
+""" 연습문제 1 기억하나 안하나 테스트 통과 """
+# number = int(input())
+
+# i = 0
+# while i < number :
+#     print('', number)
+#     i = i + 1
+
+""" 연습문제 2 기억하나 안하나 테스트 통과 """
+# number = int(input())
+
+# a = 1
+# while a <= number :
+#     print(a , a * a)
+#     a += 1
+
